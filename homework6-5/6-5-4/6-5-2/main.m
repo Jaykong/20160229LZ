08addd51b710d71d0100eac11823e1dc67094a2c
brookzero <me@brookzero.com>
2016-03-08 19:20:18 +0800

    艰难的debug了6.5.4

--- homework6-5/6-5-4/6-5-2/main.m
+++ homework6-5/6-5-4/6-5-2/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  6-5-2
+//  6-5-4
 //
 //  Created by Uno on 16/3/8.
 //  Copyright © 2016年 BrookZero. All rights reserved.
@@ -11,49 +11,49 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-    while (0) {
-        double value2;
-        char operator;
+        NSLog(@"Begin calculations, input a number and S to begin.");
         Calculator *calc = [[Calculator alloc]init];
-        NSLog(@"Begin calculations");
-        scanf("%lf %c",&value2,&operator);
-        
-
-        if (operator == 'S') {
-            [calc setAccumulator:value2];
-            continue;
-            }
-        if (operator == '+') {
-            [calc add: value2];
-            [calc print];
-            continue;
-        }
-        else if (operator == '-') {
-            [calc minus: value2];
-            [calc print];
-            continue;
-        }
-        else if (operator == '*' || operator == 'x') {
-            [calc multiply: value2];
-            [calc print];
-            continue;
-        }
-        else if (operator == '/') {
-            if ( value2 == 0) {
-                NSLog(@"你会不会算术? 0能不能被除??");
-                break;
-            }
-            else {
-                [calc divide: value2];
-                [calc print];
-                continue;
+        double calNum = 0,zoidberg = 0,getNum;
+        char operator;
+        scanf("%lf %c",&calNum,&operator);
+        if (operator == 'S' || operator == 's') {
+            NSLog(@"Let's start with number %lf.",calNum);
+            [calc setAccumulator:calNum];
+            while (zoidberg == 0) {
+                scanf("%lf %c",&getNum,&operator);
+                if (operator == '+') {
+                    [calc add: getNum];
+                    [calc print];
+                }
+                else if (operator == '-') {
+                    [calc minus: getNum];
+                    [calc print];
+                }
+                else if (operator == '*' || operator == 'x') {
+                    [calc multiply: getNum];
+                    [calc print];
+                }
+                else if (operator == '/') {
+                    if ( getNum == 0) {
+                        NSLog(@"Nooooooooo!!!! Please dont divide a number by ZERO!!!!!");
+                    }
+                    else {
+                        [calc divide: getNum];
+                        [calc print];
+                    }
+                }
+                else if (operator == 'E'||operator == 'e') {
+                    NSLog(@"End of calculations.");
+                    zoidberg = 1;
+                }
+                else {
+                    NSLog(@"你打的什么劳什子玩意?重新来!");
+                }
             }
         }
         else {
-            NSLog(@"你打的什么劳什子玩意?");
-            break;
+            NSLog(@"输入错误, 程序终止.");
         }
-
-        }}
+    }
     return 0;
 }
